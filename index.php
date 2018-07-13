7614ce7ca971e5c84a863ba36ac2c00f1e539891
mentosuncle <39184675+mentosuncle@users.noreply.github.com>
2018-07-14 00:02:23 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php header("Location: https://www.amazon.com/Furniture-Booties-Protect-Floor-Scratches/dp/B01LYRA66E"); die(); ?>
+<?php header("Location: https://www.amazon.com/AmazonBasics-Mid-Back-Office-Chair-White/dp/B0735X2M1J"); die(); ?>
